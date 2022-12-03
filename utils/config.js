a3beb859b1653f5f496aadbffb2cef5bc63e3807
Mayo <mayolito.waje08@gmail.com>
2022-12-03 15:19:26 +0800

    refactor config and logger

--- utils/config.js
+++ utils/config.js
@@ -2,4 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const { MONGODB_URI, PORT } = process.env;
+export const MONGODB_URI = process.env.NODE_ENV === 'production'
+  ? process.env.MONGODB_URI
+  : process.env.MONGODB_TEST_URI;
+
+export const { PORT } = process.env;
