28ecec64503c818d5d765e17ef847f28bc06cb8b
Mayo <mayolito.waje08@gmail.com>
2022-12-07 21:38:32 +0800

    add endpoints for users api

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import 'express-async-errors';
 import User from '../models/user.js';
 
 const usersRouter = express.Router();
@@ -6,6 +8,27 @@ const usersRouter = express.Router();
 usersRouter.get('/', async (req, res) => {
   const users = await User.find({});
   res.json(users);
+});
+
+usersRouter.post('/', async (req, res) => {
+  const { username, name, password } = req.body;
+
+  const validPasswordRegex = /^(?=.*[A-Z])(?=.*[@$!%*#?&])(?=.*[a-z])(?=.*\d).{8,}$/g;
+  const validPassword = validPasswordRegex.test(password);
+
+  if (!validPassword) {
+    return res.status(400).json({
+      error: 'password should contain one: small letter, capital letter, number, and special character',
+    });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const newUser = new User({ username, name, passwordHash });
+  const createdUser = await newUser.save();
+
+  return res.status(201).json(createdUser);
 });
 
 export default usersRouter;
--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -20,6 +20,71 @@ describe('fetching users', () => {
   });
 });
 
+describe('creating users', () => {
+  test('should successfully create user with valid details', async () => {
+    const newUser = {
+      username: 'newUser',
+      name: 'test newUser',
+      password: 'newUser#001',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(helper.users.length + 1);
+  });
+
+  test('request body should contain username, name, and password fields', async () => {
+    const badRequests = [
+      {
+        name: 'test',
+        password: 'testPassword#001',
+      },
+      {
+        username: 'test',
+        password: 'testPassword#001',
+      },
+      {
+        name: 'test',
+        username: 'test',
+      },
+    ];
+
+    await Promise.all(badRequests.map(async (badRequest) => {
+      await api
+        .post('/api/users')
+        .send(badRequest)
+        .expect(400);
+
+      const usersAtEnd = await helper.usersInDb();
+      expect(usersAtEnd).toHaveLength(helper.users.length);
+    }));
+  });
+
+  test('password should contain at least one small letter, capital letter, number, and special character', async () => {
+    const badPasswordRequest = {
+      username: 'test',
+      name: 'test',
+      password: 'badpassword',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(badPasswordRequest)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toBe('password should contain one: small letter, capital letter, number, and special character');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(helper.users.length);
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
